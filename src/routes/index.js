23d207fdddee981e796eb7213147fb7efa476231
Mélanie <mlanie.parry@gmail.com>
2020-03-02 12:52:51 +0100

    début fonctionnalité authentification

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const cocktailsRouter = require("./cocktails_router");
+const authRouter = require("./auth_router");
 
 const mainRouter = express.Router();
 
 mainRouter.use("/cocktails", cocktailsRouter);
+mainRouter.use("/auth", authRouter);
 
 module.exports = mainRouter;
