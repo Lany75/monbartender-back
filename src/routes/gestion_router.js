20a745662da22f6c17c6b33966cb12e6e6ef9783
Mélanie <mlanie.parry@gmail.com>
2020-04-20 10:44:36 +0200

    début gestion admin

--- src/routes/gestion_router.js
+++ src/routes/gestion_router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const verifyToken = require("../middlewares/verify_token");
 
 const {
   recupererIdCocktail,
@@ -12,39 +13,43 @@ const { OK, NOT_FOUND } = require("../helpers/status_code");
 
 const gestionRouter = express.Router();
 
-gestionRouter.put("/cocktails-du-moment", async (request, response) => {
-  const nomAncienCocktail = request.query.nomAncienCocktail;
-  const nomNouveauCocktail = request.query.nomNouveauCocktail;
+gestionRouter.put(
+  "/cocktails-du-moment",
+  verifyToken,
+  async (request, response) => {
+    const nomAncienCocktail = request.query.nomAncienCocktail;
+    const nomNouveauCocktail = request.query.nomNouveauCocktail;
 
-  const idAncienCocktail = await recupererIdCocktail(nomAncienCocktail);
-  const idNouveauCocktail = await recupererIdCocktail(nomNouveauCocktail);
+    const idAncienCocktail = await recupererIdCocktail(nomAncienCocktail);
+    const idNouveauCocktail = await recupererIdCocktail(nomNouveauCocktail);
 
-  await modifierCocktailMoment(idAncienCocktail, idNouveauCocktail);
+    await modifierCocktailMoment(idAncienCocktail, idNouveauCocktail);
 
-  const cocktailsMoment = [];
+    const cocktailsMoment = [];
 
-  const idCocktailsMoment = await recupererIdCocktailsMoment();
+    const idCocktailsMoment = await recupererIdCocktailsMoment();
 
-  if (!idCocktailsMoment) {
-    response
-      .status(NOT_FOUND)
-      .json("La liste de cocktail n'a pas été récupérée");
-  } else {
-    for (let i = 0; i < idCocktailsMoment.length; i++) {
-      const cocktail = await recupererUnCocktail(
-        idCocktailsMoment[i].cocktail_id
-      );
+    if (!idCocktailsMoment) {
+      response
+        .status(NOT_FOUND)
+        .json("La liste de cocktail n'a pas été récupérée");
+    } else {
+      for (let i = 0; i < idCocktailsMoment.length; i++) {
+        const cocktail = await recupererUnCocktail(
+          idCocktailsMoment[i].cocktail_id
+        );
 
-      cocktailsMoment.push({
-        id: idCocktailsMoment[i].cocktail_id,
-        nom: cocktail.dataValues.nom,
-        photo: cocktail.dataValues.photo
-      });
+        cocktailsMoment.push({
+          id: idCocktailsMoment[i].cocktail_id,
+          nom: cocktail.dataValues.nom,
+          photo: cocktail.dataValues.photo
+        });
+      }
     }
-  }
 
-  response.status(OK);
-  response.json(cocktailsMoment);
-});
+    response.status(OK);
+    response.json(cocktailsMoment);
+  }
+);
 
 module.exports = gestionRouter;
