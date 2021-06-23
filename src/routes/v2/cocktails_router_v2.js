19d566ee2dd411b059892e75de0797db9fbe7b52
Lany75 <mlanie.parry@gmail.com>
2021-06-23 15:37:06 +0200

    F2 - api V2 - Create get /:name router and getAllNameCocktails controller

--- src/routes/v2/cocktails_router_v2.js
+++ src/routes/v2/cocktails_router_v2.js
@@ -3,7 +3,8 @@ const express = require("express");
 const logger = require("../../helpers/logger");
 
 const {
-  getAllCocktails
+  getAllCocktails,
+  getAllNameCocktails
 } = require('../../controllers/v2/cocktails_controller_v2');
 
 const cocktailsRouterV2 = express.Router();
@@ -13,6 +14,13 @@ cocktailsRouterV2.get('/', async (request, response) => {
   logger.info(`Trying to get all cocktails`);
   const cocktails = await getAllCocktails();
 
+  response.status(200).json(cocktails);
+})
+
+cocktailsRouterV2.get('/:name', async (request, response) => {
+  const { name } = request.params;
+  logger.info(`Trying to get cocktail ${name}`);
+  const cocktails = await getAllNameCocktails(name);
   response.status(200).json(cocktails);
 })
 
