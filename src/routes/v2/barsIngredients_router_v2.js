b276ae58ed3c943e98c8138b094ee3f667cc5b23
Lany75 <mlanie.parry@gmail.com>
2021-08-25 15:39:09 +0200

    F70 - Move post ingredient in bar into barsIngredients router

--- src/routes/v2/barsIngredients_router_v2.js
+++ src/routes/v2/barsIngredients_router_v2.js
@@ -4,9 +4,18 @@ const isAuthenticated = require("../../middlewares/is_authenticated");
 const logger = require("../../helpers/logger");
 
 const { getUserBar } = require("../../controllers/v2/bars_controller_v2");
-const { deleteIngredientFromUserBar } = require("../../controllers/v2/barsIngredients_controller_v2");
 
-const { BAD_REQUEST, OK } = require("../../helpers/status_code");
+const {
+  postIngredientInUserBar,
+  ingredientAlreadyInBar,
+  deleteIngredientFromUserBar
+} = require("../../controllers/v2/barsIngredients_controller_v2");
+
+const {
+  idIsExisting
+} = require('../../controllers/v2/ingredients_controller_v2');
+
+const { BAD_REQUEST, OK, CREATED } = require("../../helpers/status_code");
 
 const barsIngredientsRouterV2 = express.Router();
 
@@ -37,7 +46,35 @@ barsIngredientsRouterV2.delete('/', isAuthenticated, async (request, response) =
         .json("Bar inexistant");
     }
   }
+})
 
+barsIngredientsRouterV2.post('/', isAuthenticated, async (request, response) => {
+  const mail = request.user.email;
+  const { ingredientId } = request.body;
+
+  if (await idIsExisting(ingredientId)) {
+    logger.info(`Trying to get ${mail}'s bar`);
+    let bar = await getUserBar(mail);
+
+    if (bar) {
+      logger.info(`Verifying ingredient not in user's bar`);
+      if (!await ingredientAlreadyInBar(ingredientId, bar.dataValues.id)) {
+        logger.info(`Trying to post ingredient in user's bar`);
+        await postIngredientInUserBar(ingredientId, bar.dataValues.id);
+      }
+
+      bar = await getUserBar(mail);
+      response.status(CREATED).json(bar);
+    } else {
+      response
+        .status(BAD_REQUEST)
+        .json("Bar inexistant");
+    }
+  } else {
+    response
+      .status(BAD_REQUEST)
+      .json("Incorrect id");
+  }
 })
 
 module.exports = barsIngredientsRouterV2;
