91ac34252782bd6e7b6f26d327c4006d80467897
Lany75 <mlanie.parry@gmail.com>
2021-11-03 15:44:14 +0100

    F44 - Post new cocktail in database

--- src/controllers/v2/cocktailsIngredients_controller_v2.js
+++ src/controllers/v2/cocktailsIngredients_controller_v2.js
@@ -9,6 +9,32 @@ const cocktailsIngredientControllerV2 = {
 
     if (ingredient) return true;
     else return false;
+  },
+
+  unityIsUsed: async (unityName) => {
+    const ingredient = await CocktailIngredient.findOne({
+      attributes: ['ingredientId'],
+      where: { unite: unityName }
+    })
+
+    if (ingredient) return true;
+    else return false;
+  },
+
+  bindCocktailIngredient: async (idCocktail, idIngredient, quantite, unite) => {
+    if (quantite === "" || unite === "") {
+      await CocktailIngredient.create({
+        cocktailId: idCocktail,
+        ingredientId: idIngredient
+      });
+    } else {
+      await CocktailIngredient.create({
+        cocktailId: idCocktail,
+        ingredientId: idIngredient,
+        quantite: quantite,
+        unite: unite
+      });
+    }
   }
 }
 
